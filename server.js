0cd2a53fd954a38e4dfc9e2e6bdd52c765eb0920
Praman <Pruhmon@gmail.com>
2025-06-15 22:06:08 +0000

    Done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //include Express
 const express = require('express');
-
+const http = require('http');
 //set port
 const port = 3000;
 
@@ -8,15 +8,15 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send(`
-	    <h1>Here's my home page</h1>
-      <p>Welcome to Express!</p>
+	    <h1>Praman's my home page</h1>
+      <p>Welcome to the P party!</p>
   `);
 
 });
 
 app.get('/about',(req,res)=>{
     res.send(`
-	    <h1>Here's Bill's Express About Page</h1>
+	    <h1>Here's Praman's Express About Page</h1>
       <p>Welcome to Express!</p>
   `);
 
